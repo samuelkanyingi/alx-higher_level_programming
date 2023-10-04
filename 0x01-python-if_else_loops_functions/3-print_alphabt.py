868f2785358169bbe444d0005715ecf9a0b90e87
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-10-03 18:27:23 -0700

    if/else, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for askey in range(ord('a'), ord('z')+1):
     if chr(askey) not in ['q', 'e']:
-        print(chr(askey), end='')
+        print('{}'.format(chr(askey)), end='')
