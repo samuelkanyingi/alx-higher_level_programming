6d9efc5933817d20e64bc285397c2186fc77d7a9
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-10-05 04:59:24 -0700

    module

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+add_res = add(a, b)
+sub_res = sub(a, b)
+multi_res = mul(a, b)
+div_res = div(a, b)
+
+print("{} + {} = {}".format(a, b, add_res))
+print("{} - {} = {}".format(a, b, sub_res))
+print("{} * {} = {}".format(a, b, multi_res))
+print("{} / {} = {}".format(a, b, div_res))
