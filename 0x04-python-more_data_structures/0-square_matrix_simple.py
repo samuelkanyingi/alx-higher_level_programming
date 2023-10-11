f50f8219e6035358186eaa0c6e943b3095f9259a
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-10-11 01:54:58 -0700

    square

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for j in row:
+            s = j  ** 2
+            new_row.append(s)
+        new_matrix.append(new_row)
+    return new_matrix
