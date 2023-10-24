436cc916aa0aade242bee765521a2b661cfd9ed1
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-10-24 05:03:05 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+define class
+"""
+
+
+class Square:
+    """define class properties"""
+    def __init__(self, size):
+        """
+        size: The size of the square
+        __size: private size of square
+        """
+        self.__size = size
