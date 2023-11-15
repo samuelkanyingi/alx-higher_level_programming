a41859785a76ac9936f751601e2ff32ab6aec5d6
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-11-14 21:53:00 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,15 @@ class Square(Rectangle):
 
     """Class Square inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """intializee square"""
+        """intialize square
+        Args:
+            size: length of each side of rectangle
+            x: return x cordinate of rectangle
+            y: return y cordinate of rectangle
+            id: identifier of rectangle
+        Returns:
+            None
+        """
         super().__init__(size, size, x, y, id)
 
     @property
@@ -17,12 +25,21 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """size setter"""
+        """size setter
+        Args:
+            Value: The new length value
+        Return:
+            None
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """update attributes with no-keyword and kwargs"""
+        """update attributes with no-keyword and kwargs
+        Args:
+            *args: Positional arguments to show attributes
+            **kwargs: Keyword arguments to show attributes
+        """
         if args:
             attributes = ['id', 'size', 'x', 'y']
             for i in range(min(len(args), len(attributes))):
