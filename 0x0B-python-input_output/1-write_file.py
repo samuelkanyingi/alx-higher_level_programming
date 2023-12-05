c322a1947b251e2a291b98b08c7265222bf8585c
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-12-05 04:29:48 -0800

    input

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module to find method"""
 
+
 def write_file(filename="", text=""):
     """writes a string to a text file"""
     with open(filename, 'w', encoding='utf-8') as file:
