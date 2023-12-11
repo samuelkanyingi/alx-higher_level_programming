3eef6828e1c9f1d4ff70c8e3836a0bdb404abde0
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-12-11 11:51:51 -0800

    tdd

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """module to find the class"""
+import json
 
 
 class Base:
@@ -13,3 +14,55 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """method that returns the JSON string representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """method that writes the JSON string of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+        class_name = cls.__name__
+        filename = f"{class_name}.json"
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dicts)
+        with open(filename, 'w') as file:
+            file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """method that returns the list of the JSON string representation"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """method that returns an instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            _inst = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy_inst = cls(1)
+        else:
+            raise ValueError("Unsupported class")
+
+        dummy_inst.update(**dictionary)
+        return dummy_inst
+
+    @classmethod
+    def load_from_file(cls):
+        """method that returns a list of instances"""
+        filename = f"{cls.__name__}.json"
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+                list_dicts = cls.from_json_string(json_string)
+                return [cls.create(**data) for data in list_dicts]
+        except FileNotFoundError:
+            return []
