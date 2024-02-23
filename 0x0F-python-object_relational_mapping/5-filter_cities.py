e99f30b5759ba9b2183db823c1a08324fdd9408f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-02-23 02:56:34 -0800

    sql injection free

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    state_name = argv[4]
-    cur.execute("SELECT cities.name FROM cities \
+    sql_query = "SELECT cities.name FROM cities \
             JOIN states ON cities.state_id=states.id \
             WHERE states.name=%s \
-            ORDER BY cities.id ASC", (state_name,))
+            ORDER BY cities.id ASC"
+    state_name = argv[4]
+    cur.execute(sql_query, (state_name,))
     cities = cur.fetchall()
     print(", ".join(city[0] for city in cities))
     cur.close()
