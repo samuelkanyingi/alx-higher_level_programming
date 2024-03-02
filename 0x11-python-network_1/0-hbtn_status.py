f50c7b8f825a87d2875ed23c400c107a181b0ec0
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-01 18:36:07 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        body = r.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
