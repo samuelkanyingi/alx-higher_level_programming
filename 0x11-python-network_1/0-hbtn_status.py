89a804e5d6a7709cd7f6eec06176574c289b5bd0
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-01 18:40:11 -0800

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 if __name__ == '__main__':
     import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
