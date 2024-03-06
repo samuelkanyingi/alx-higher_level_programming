4e27ac1e5045fdd74aa2b1089659f7357e028f9e
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-05 18:53:16 -0800

    Post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that displays the body of the response"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+         body = response.read().decode('utf-8')
+        print(body)
