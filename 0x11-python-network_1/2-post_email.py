f755338c604f16f7e291cd8ab2d7f787565a8bb2
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-12 02:48:05 -0700

    Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
     with urllib.request.urlopen(url, data=data) as response:
-         body = response.read().decode('utf-8')
-         print(body)
+        body = response.read().decode('utf-8')
+        print(body)
