d1660626b9b8f098b447db96c2fd16ac15b1c876
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-12 02:54:43 -0700

    Fetch  status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,6 @@ if __name__ == '__main__':
     import requests
     # Send a GET request to the URL
     response = requests.get('https://alx-intranet.hbtn.io/status')
-
-
     print('Body response:')
     print('\t- type:', type(response.text))
     print('\t- content:', response.text)
