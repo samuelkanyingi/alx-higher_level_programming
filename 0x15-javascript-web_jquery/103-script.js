91953869b0f03c9c01f0652a7e268070c7e6903e
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-08 04:35:17 -0700

    And press ENTER

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  function translateHello () {
+    const languageCode = $('#language_code').val();
+    $.get('https://www.fourtonfish.com/hellosalut/hello/', { lang: languageCode }, function (data) {
+      $('#hello').text(data.hello);
+    });
+  }
+
+  $('#btn_translate').click(translateHello);
+
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) { // Check if Enter key is pressed
+      translateHello();
+    }
+  });
+});
