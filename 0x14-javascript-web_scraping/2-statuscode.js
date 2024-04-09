cac5fd87ddfc362b80285329161f4ac65b193f17
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-09 12:57:17 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,11 +2,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, function (response) {
-  if (response.statusCode === 200) {
-    console.log('code: ', response.statusCode);
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
   } else {
     console.log('code: ', response.statusCode);
   }
-}
-);
+});
