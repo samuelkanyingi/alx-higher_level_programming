43dd8007716f67912e752dae67115de56c33c57f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-09 20:18:29 -0700

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request.get(url, function (error, response) {
   if (error) {
     console.log(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:' + ' ' + response.statusCode);
   }
 });
