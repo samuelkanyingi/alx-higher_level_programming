8c29dc7466ad060adf9cd7e8107558e283f0d6b5
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-09 21:54:58 -0700

    scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,20 +5,20 @@ const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
-  } else {
-    const todos = JSON.parse(body);
-    const completedTasksByUserId = {};
-
-    todos.forEach(todo => {
-      if (todo.completed) {
-        if (completedTasksByUserId[todo.userId]) {
-          completedTasksByUserId[todo.userId]++;
-        } else {
-          completedTasksByUserId[todo.userId] = 1;
-        }
-      }
-    });
-
-    console.log(completedTasksByUserId);
   }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUserId = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (!completedTasksByUserId[todo.userId]) {
+        completedTasksByUserId[todo.userId] = 1;
+      } else {
+        completedTasksByUserId[todo.userId] += 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUserId);
 });
