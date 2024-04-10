042c8e3a03699c79e6a11a35df4b34846e6899d5
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-09 20:06:20 -0700

    web scraping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Extract the movie ID from command-line arguments
+const movieId = process.argv[2];
+
+// Construct the URL for the Star Wars API endpoint
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status Code:', response.statusCode);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log('Title:', movieData.title);
+  }
+});
